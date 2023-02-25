7ecd2c069cf10471e250d8cc9befed93fae6bbdf
mWalrus <hellkvistoskar@protonmail.com>
2023-02-25 16:00:16 +0100

    fix: display current version in version prompt

--- release.sh
+++ release.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 VERSION_REGEX="[0-9]+\.[0-9]+\.[0-9]+(rc-[0-9]+)?"
+INFO_FILE="./info.toml"
 
-printf "Version (ex. 1.10.2): " >&2
+CURRENT_VERSION=$(grep -E -o "$VERSION_REGEX" $INFO_FILE)
+printf "Version (currently %s): " "$CURRENT_VERSION" >&2
 read -r VERSION
 
-sed -E -i "2s/$VERSION_REGEX/$VERSION/I" ./info.toml
+sed -E -i "2s/$VERSION_REGEX/$VERSION/I" $INFO_FILE
 
 command -v zip > /dev/null && zip "./releases/MapRank-v$VERSION.op" ./*.as ./info.toml || printf "Failed to build release archive"
