289c14a77fffce31c8fe761b941b285a89e45ebc
mWalrus <hellkvistoskar@protonmail.com>
2023-08-22 01:08:55 +0200

    chore: update build dev script

--- build-dev.sh
+++ build-dev.sh
@@ -1,4 +1,10 @@
 mkdir "$PWD/dev-builds"
+
+cp ./info.toml ./info.toml.old
+sed -E -i "3s/Map Rank/Map Rank Dev/I" ./info.toml
+
 command -v zip > /dev/null && zip "$PWD/dev-builds/MapRank-Dev.op" ./*.as ./info.toml || printf "Failed to build release archive"
 cp -f "$PWD/dev-builds/MapRank-Dev.op" "$HOME/.steam/steam/steamapps/common/Trackmania/Openplanet/Plugins/"
+
+mv ./info.toml.old ./info.toml
 
