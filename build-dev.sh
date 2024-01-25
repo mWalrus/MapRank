74d29014e91209b773da1be41aa6818be379e482
mWalrus <hellkvistoskar@protonmail.com>
2024-01-25 18:05:30 +0100

    fix: update dev build script

--- build-dev.sh
+++ build-dev.sh
@@ -1,10 +1,19 @@
-mkdir "$PWD/dev-builds"
+DEV_DIR="$PWD/dev-builds"
+ARCHIVE_PATH="$DEV_DIR/MapRank-Dev.op"
+
+if [ ! -d "$DEV_DIR" ]; then
+  mkdir "$DEV_DIR"
+fi
 
 cp ./info.toml ./info.toml.old
 sed -E -i "3s/Map Rank/Map Rank Dev/I" ./info.toml
 
-command -v zip > /dev/null && zip "$PWD/dev-builds/MapRank-Dev.op" ./*.as ./info.toml || printf "Failed to build release archive"
-cp -f "$PWD/dev-builds/MapRank-Dev.op" "$HOME/.steam/steam/steamapps/common/Trackmania/Openplanet/Plugins/"
+if [ -f "$ARCHIVE_PATH" ]; then
+  rm -rf "$ARCHIVE_PATH"
+fi
+
+command -v zip > /dev/null && zip "$ARCHIVE_PATH" ./src/*.as ./info.toml || printf "Failed to build release archive"
+cp -f "$ARCHIVE_PATH" "$HOME/.steam/steam/steamapps/compatdata/2225070/pfx/drive_c/users/steamuser/OpenplanetNext/Plugins"
 
 mv ./info.toml.old ./info.toml
 
