c91400d8f8b11f914ccd148eac63abcc1648bfc9
mWalrus <hellkvistoskar@protonmail.com>
2024-01-25 18:12:25 +0100

    fix: update release script to match project structure changes

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VERSION_REGEX="[0-9]+\.[0-9]+\.[0-9]+(rc-[0-9]+)?"
+VERSION_REGEX="[0-9]+\.[0-9]+(\.[0-9]+)?(rc-[0-9]+)?"
 INFO_FILE="./info.toml"
 
 CURRENT_VERSION=$(grep -E -o "$VERSION_REGEX" $INFO_FILE)
@@ -8,4 +8,10 @@ read -r VERSION
 
 sed -E -i "2s/$VERSION_REGEX/$VERSION/I" $INFO_FILE
 
-command -v zip > /dev/null && zip "./releases/MapRank-v$VERSION.op" ./*.as ./info.toml || printf "Failed to build release archive"
+ARCHIVE_PATH="$PWD/releases/MapRank-v$VERSION.op"
+
+if [ -f "$ARCHIVE_PATH" ]; then
+  rm -rf "$ARCHIVE_PATH"
+fi
+
+command -v zip > /dev/null && zip "$ARCHIVE_PATH" ./src/*.as ./info.toml || printf "Failed to build release archive"
